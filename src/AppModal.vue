a59577fdcca693a28e0d07290b03bb8d253ed8a8
iamrootjs <136985154+iamrootjs@users.noreply.github.com>
2023-12-05 20:55:10 +0200

    Initial Commit

---
+++ src/AppModal.vue
@@ -0,0 +1,8 @@
+<script setup>
+import { XMarkIcon } from "@heroicons/vue/20/solid";
+// Start your component logic here
+</script>
+
+<template>
+  <!--  Start your component template here -->
+</template>
